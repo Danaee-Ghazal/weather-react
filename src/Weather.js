81caf92c0f31632f377a85cf5f94d099463291ab
Ghazal-Danaei <ghazal.danaei78@gmail.com>
2023-08-03 18:11:24 +0330

    created JS and CSS files in React

---
+++ src/Weather.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import axios from "axios";
+export default function Weather() {
+  return (
+    <div className="container">
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <form>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Type the city's name please..."
+                  className="form-control"
+                  autoComplete="off"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-warning w-100"
+                />
+              </div>
+            </div>
+          </form>
+          <div className="overview">
+            <h1></h1>
+            <ul>
+              <li>
+                Last updated: <span></span>
+              </li>
+              <li></li>
+            </ul>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix weather-temperature">
+                <img src="" alt="Clear" className="float-left" />
+                <div className="float-left">
+                  <strong></strong>
+                  <span className="units"> °C </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  Humidity: <span></span>%
+                </li>
+                <li>
+                  Wind: <span></span> km/h
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="weather-forecast"></div>
+        </div>
+        <small>
+          <a
+            href="https://github.com/Danaee-Ghazal/vanilla-weather-app/tree/main"
+            target="_blank"
+          >
+            Open-source code
+          </a>
+          by Ghazal Danaee
+        </small>
+      </div>
+    </div>
+  );
+}
